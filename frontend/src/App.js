8b953f25839c18351630949e5a69b75740317e22
mohamed <mohamed.dridi@fernride.com>
2025-02-19 14:16:26 +0100

    use create react app to generate a react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p> Welcome to EV management</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
