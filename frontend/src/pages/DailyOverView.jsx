85dc64ce0615e00cfb32b96e7aaa49a26816024c
Mohamed Habib Dridi <48842413+medhabibdridi@users.noreply.github.com>
2025-02-24 00:07:29 +0100

    Update DailyOverView.jsx

--- frontend/src/pages/DailyOverView.jsx
+++ frontend/src/pages/DailyOverView.jsx
@@ -4,13 +4,12 @@ import LineChartCard from '../components/LineChartCard';
 import data from '../data/MockChartsData.json';
 
 const DailyOverView = () => {
-  const totalEvents = 132;
 
   return (
     <div className="p-4">
       <div className="flex flex-wrap lg:flex-nowrap">
         <div className='w-full lg:w-1/3 flex flex-col gap-5 mb-5 lg:mb-0'>
-          <NumberCard title="Number Of Charging Events " number={totalEvents} />
+          <NumberCard title="Number Of Charging Events " number={132} />
           <NumberCard title="Current Concurrency Factor (%)" number={55} />
           <NumberCard title="Total Energy Charged (Kwh)" number={12345} />
         </div>
